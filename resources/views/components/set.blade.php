1a670a756a6a3710aceffc614606f32c6e9ddcbc
Aaron Belz <belzaaron@gmail.com>
2021-08-05 14:12:58 -0400

    Add small help attribute for sets

--- resources/views/components/set.blade.php
+++ resources/views/components/set.blade.php
@@ -1,10 +1,17 @@
 <x-booty-group>
     <x-booty-label :value="$label" :invalid="$errors->has($name)" />
+    
+    @if ($attributes->has('help'))
+        <small class="form-text text-muted">
+            {{ $attributes->get('help') }}
+        </small>
+    @endif
+    
     @if ($slot->isNotEmpty())
         <div class="input-group">
     @endif
-
-    <x-booty-input :name="$name" :invalid="$errors->has($name)" {{ $attributes }} />
+    
+    <x-booty-input :name="$name" :invalid="$errors->has($name)" {{ $attributes->execpt(['help']) }} />
 
     @if ($slot->isNotEmpty())
         {{ $slot }}
